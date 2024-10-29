a8bda9ccb6d545bf54a05d866190b542303c35ac
lchen7809 <160735650+lchen7809@users.noreply.github.com>
2024-10-29 09:47:35 +0800

    initilise frontend with React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tic-Tac-Toe game
         </p>
         <a
           className="App-link"
